76018f71326656bf024f4584d293b6a9fbb5e994
Veselin Iliev <veneli93@gmail.com>
2026-05-10 16:45:24 +0300

    updating middleware to return tenat.id instead of the api-key

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -1,10 +1,11 @@
 from db.storage import temp_db_tenants
 from models.tenant import Tenant
-from fastapi import HTTPException, Header
+from fastapi import HTTPException, Header, Request
 
-async def verify_api_key(api_key: str | None = Header(default=None)) -> Tenant:
+async def verify_api_key(request: Request ,api_key: str | None = Header(default=None)) -> Tenant:
     tenant = next((tenant for tenant in temp_db_tenants if tenant.api_key == api_key), None)
     if not tenant:
         raise HTTPException(status_code=401, detail="Invalid API key")
 
+    request.state.tenant_id = tenant.id
     return tenant
