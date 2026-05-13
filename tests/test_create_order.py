5fb1519acfd698a0eb4aa4e2d105481d2d7f2671
Veselin Iliev <veneli93@gmail.com>
2026-05-13 21:20:02 +0300

    adding test for order_history

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -3,6 +3,10 @@ from httpx import AsyncClient
 
 from models.order import Order
 from models.order_status import OrderStatus
+from models.order_status_history import OrderStatusHistory
+from sqlalchemy import select
+
+
 
 
 async def create_tenant_to_get_api_key(client: AsyncClient) -> tuple[str, int]:
@@ -34,3 +38,11 @@ async def test_create_order(client: AsyncClient, db_session: AsyncSession) -> No
     assert record.status == OrderStatus.PENDING
     assert record.priority == 4
     assert record.created_at is not None
+
+    history_record = select(OrderStatusHistory).where(
+        OrderStatusHistory.order_id == record.id
+    )
+    history_record = (await db_session.execute(history_record)).scalars().first()
+    assert history_record is not None
+    assert history_record.status == OrderStatus.PENDING
+    assert history_record.order_id == order_response.json()["id"]
