0d36c0fc6afc091e12af4d2a1bc095477e2314cc
Veselin Iliev <veneli93@gmail.com>
2026-05-17 01:27:46 +0300

    added a parametrize test case

--- tests/system/test_system_create_order.py
+++ tests/system/test_system_create_order.py
@@ -1,3 +1,4 @@
+import pytest
 from httpx import AsyncClient
 
 from schemas.order import OrderResponse
@@ -15,3 +16,33 @@ async def test_create_order(
     )
     assert order_response.status_code == 200
     OrderResponse.model_validate(order_response.json())
+
+
+@pytest.mark.parametrize(
+    "price,expected_status",
+    [
+        (50.0, 200),
+        (150.0, 400),
+        (100.0, 200),
+        (
+            0.0,
+            200,
+        ),
+        (
+            None,
+            422,
+        ),
+    ],
+)
+async def test_price_expected_status(
+    client: AsyncClient, tenant_credentials: tuple[str, int], price, expected_status
+):
+
+    body = {} if price is None else {"price": price}
+    api_key, tena_id = tenant_credentials
+    order_response = await client.post(
+        f"/tenants/{tena_id}/orders/",
+        json=body,
+        headers={"api-key": api_key},
+    )
+    assert order_response.status_code == expected_status
